244f8e88dec2e9689f26b6a89216892ec3c6b58f
Andre <andre.luismp@gmail.com>
2024-01-20 15:00:17 +0000

    typescript: SearchBar

--- client/src/searchComponents/SearchBar.tsx
+++ client/src/searchComponents/SearchBar.tsx
@@ -1,20 +1,26 @@
-import React from "react";
-import { BsSearchHeart } from "react-icons/bs";
+'use strict';
 
-export function SearchBar ({ search, handleSearch, setSearch }) {
+import { BsSearchHeart } from "react-icons/bs";
+import { Search } from "react-router-dom";
+
+interface SearchBarProps {
+  search: string,
+  handleSearch: Function,
+  setSearch: Function
+}
+
+export const SearchBar: React.FC<SearchBarProps> = ({ search, handleSearch, setSearch }) => {
   return (
     <form className="searchForm" onSubmit={(e) => e.preventDefault()} >
       <input
         type="text"
-        size="50"
-        name=""
         id="search"
         role="searchbox"
         placeholder=" Find music like..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <button onClick={handleSearch} type="submit" id="submitButton">
+      <button onClick={() => handleSearch} type="submit" id="submitButton">
         <BsSearchHeart />
       </button>
     </form>
