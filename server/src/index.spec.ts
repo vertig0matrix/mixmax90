66c964ebaf3edf921f9f5ff8c2f11765eaaf9336
Andre <andre.luismp@gmail.com>
2024-01-22 20:31:16 +0000

    test: set up and database test

---
+++ server/src/index.spec.ts
@@ -0,0 +1,11 @@
+import request from 'supertest';
+import { app } from './index';
+
+describe('Test the root path', () => {
+    test('It should respond to the GET method', async () => {
+        const res = await request(app)
+            .get("/");
+
+        expect(res.statusCode).toBe(200);
+    });
+});
