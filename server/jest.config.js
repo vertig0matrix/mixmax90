fb7184f5fe96995709cf6841b83efb87124906b4
Andre <andre.luismp@gmail.com>
2024-01-23 10:39:41 +0000

    test

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  preset: "@shelf/jest-mongodb",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {}
+    ]
+  },
   testEnvironment: 'node',
 };
 
--- server/src/index.model.test.js
+++ server/src/index.model.test.js
@@ -3,22 +3,35 @@ const mongoose = require("mongoose");
 const topTracksModel = require("./models/topTracksModel.models");
 
 describe("DB Connection", () => {
+    let connection;
+    let db;
+
     beforeAll(async () => {
-        await mongoose.connect(process.env.MONGO_URL)
+        connection = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        db = await connection.db(process.env.MONGO_URL)
     });
+    afterAll(async () => {
+        await connection.close();
+    });
+
     test('Insert a Playlist', async () => {
-        const topTrackData = {
+
+        const playlist = db.collection('playlist')
+        const mockTrack = {
             tracks: [
                 {
                     name: 'This is a new track',
                 },
             ],
+            _id: 'some-user-id'
         };
-        const savedTopTrack = await topTracksModel.default.create(topTrackData);
+        console.log(playlist)
+        await db.insertOne(mockTrack);
+        const insertedTrack = await playlist.findOne({ _id: 'some-user-id' });
         console.log(savedTopTrack);
-        expect(savedTopTrack.tracks[0].name).toBe('This is a new track');
-    });
-    afterAll(() => {
-        mongoose.disconnect();
+        expect(insertedTrack).toBe(mockTrack);
     });
 });
