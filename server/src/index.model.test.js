4b6c8fc37482b0c9c0871d1ca9a846de3bfe1956
Andre <andre.luismp@gmail.com>
2024-01-23 10:07:20 +0000

    test: db.create

--- server/src/index.model.test.js
+++ server/src/index.model.test.js
@@ -2,18 +2,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const topTracksModel = require("./models/topTracksModel.models");
 
-describe("Connection", () => {
+describe("DB Connection", () => {
     beforeAll(async () => {
         await mongoose.connect(process.env.MONGO_URL)
     });
-
-    test("Retrieve article by Id", async () => {
-        const id = "5ff2454f94eeee0a7acb5c30";
-        const topTracks = await topTracksModel.default.find({});
-        console.log(topTracks[0].tracks[0].name);
-        expect(topTracks[0].tracks[0].name).toBe("This is another post example");
-    });
-
     test('Insert a Playlist', async () => {
         const topTrackData = {
             tracks: [
@@ -22,14 +14,11 @@ describe("Connection", () => {
                 },
             ],
         };
-        const newTopTrack = new topTracksModel.default(topTrackData);
-        const savedTopTrack = await newTopTrack.save();
+        const savedTopTrack = await topTracksModel.default.create(topTrackData);
+        console.log(savedTopTrack);
         expect(savedTopTrack.tracks[0].name).toBe('This is a new track');
-
-        afterAll(() => {
-            mongoose.disconnect();
-        });
     });
-
-
+    afterAll(() => {
+        mongoose.disconnect();
+    });
 });
