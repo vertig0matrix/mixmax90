8317b67f8fd3767af9d5b59cbcbd059c75acd3ac
Deed G <vertigomatrix@protonmail.com>
2024-01-24 15:21:45 +0000

    fix: saved collections

--- client/src/components/SavedPlaylist.tsx
+++ client/src/components/SavedPlaylist.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { TopTracks } from "../Interfaces/topTracks.interface.ts";
 import { Track, TracksWrapper } from "../Interfaces/track.interface.ts";
 
@@ -8,13 +8,25 @@ interface SavedPlaylistProps {
 
 const SavedPlaylist: React.FC<SavedPlaylistProps> = ({ playlist }) => {
   let PL = playlist.tracks
+  const [open, setOpen] = useState(false);
+
+  function toggle() {
+    setOpen(!open)
+  } 
 
   return (
-    <div>
-      {playlist.tracks.map(track =>
-        <div key={track.id}>{track.name} </div>
-      )}
+    <>
+      <div>
+      <div>
+      <button onClick={toggle} >toggle</button>
+      {open && playlist.tracks.map(track =>
+          <div key={track.id}>{track.name} </div>
+        )}
     </div>
+
+        <div>❌</div>
+      </div>
+    </>
   )
 };
 
