c993fe9443d84b88a8b8ab908c24f7faeb2f8f7f
Andre <andre.luismp@gmail.com>
2024-01-24 18:41:00 +0000

    test

--- client/src/searchComponents/SearchBar.test.tsx
+++ client/src/searchComponents/SearchBar.test.tsx
@@ -2,17 +2,25 @@ import { describe, it, expect, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SearchBar from './SearchBar.tsx';
+import { userEvent } from '@testing-library/user-event';
 
 describe('SearchBar Component', () => {
-    it('search form saves the query in the setSearch variable', () => {
+    it('search form saves the query in the setSearch variable', async () => {
         const mockSearch = "50 cents";
         const setSearch = vi.fn();
         const handleSearch = vi.fn();
+        const search = ''
+
 
         render(<SearchBar search={mockSearch} setSearch={setSearch} handleSearch={handleSearch} />)
 
-        const test = screen.getByRole('searchbox').nodeValue = mockSearch;
-        console.log(test)
-        expect(test).toBe(mockSearch);
+        const searchForm = screen.getByRole('searchbox')
+        const button = screen.getByRole('button')
+
+        userEvent.type(searchForm, mockSearch)
+        userEvent.type(searchForm, '{enter}')
+
+        await expect(handleSearch).toHaveBeenCalled();
+
     })
 })
