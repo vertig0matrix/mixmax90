18ddbe4fca6848e927abec604a8b71b0bcef8b01
Andre <andre.luismp@gmail.com>
2024-01-25 10:48:29 +0000

    test: create a playlist

--- client/src/components/Search.test.jsx
+++ client/src/components/Search.test.jsx
@@ -1,7 +1,7 @@
 import { test, vi, expect } from 'vitest'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 import Search from './Search'
-import { searchForArtist, handleRelatedArtistData } from './apiServices.js'
+import { searchForArtist, getRelatedArtistData, getArtistIds, getTopTracks, getRandomTracksByArtist } from './apiServices.js'
 
 
 vi.mock('./apiServices.js')
@@ -18,8 +18,26 @@ describe('Search Component', () => {
 
         render(<Search search="test artist" setSearch={setSearch} />)
 
-        fireEvent.submit(screen.getByRole('button'))
+        fireEvent.submit(screen.getByRole('searchbox'))
         await expect(searchForArtist).toHaveBeenCalledWith('test+artist')
         expect(setSearch).toHaveBeenCalledWith('')
+    })
+
+    test('calls get handleRelatedArtistData and get the topTracks', async () => {
+        getRelatedArtistData = vi.fn()
+        getArtistIds = vi.fn().mockResolvedValue(['123', '456'])
+        getTopTracks = vi.fn()
+        getRandomTracksByArtist = vi.fn().mockResolvedValueOnce(
+            { artists: [{ id: '123' }] }, { artists: [{ id: '456' }] }
+        )
+        const setTopTracks = vi.fn()
+
+        render(<Search setArtistId="789" getRelatedArtistData={getRelatedArtistData} getArtistIds={getArtistIds} getTopTracks={getTopTracks} getRandomTracksByArtist={getRandomTracksByArtist} />)
+
+        fireEvent.submit(screen.getByRole('heartClick'))
+
+
+
+
     })
 })
